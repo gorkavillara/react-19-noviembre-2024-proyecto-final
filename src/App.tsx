738b603ff782bbfae531e342ab12d5aa340c9421
gorkavillara <gorkavillara@gmail.com>
2024-11-11 20:21:18 +0100

    Sesión 01 - Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <p>Este es el proyecto final de Noviembre 2024</p>
+    </>
+  )
+}
+
+export default App
